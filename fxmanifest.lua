733699f3ac248729d51bf3938eee7f18192d93b9
GhzGarage <qbcore@programmer.net>
2021-04-10 21:56:13 -0500

    Initial Commit

---
+++ fxmanifest.lua
@@ -0,0 +1,28 @@
+fx_version 'cerulean'
+game 'gta5'
+
+description 'QB-VehicleSales'
+version '1.0.0'
+
+ui_page "html/ui.html"
+
+description 'QB Occasions'
+
+client_scripts {
+	'client/main.lua',
+	'config.lua',
+}
+
+server_scripts {
+	'server/main.lua',
+	'config.lua',
+}
+
+files {
+	'html/reset.css',
+	'html/new-sell-contract.png',
+	'html/new-buy-contract.png',
+	'html/ui.css',
+	'html/ui.html',
+	'html/ui.js',
+}
