8069bb9a8ff44fe63667ac375ce5a92c181fa0a6
GhzGarage <gigahertzgarage@mail.com>
2021-09-13 01:21:55 -0500

    Update SQL queries for MySQL2

--- server/main.lua
+++ server/main.lua
@@ -10,7 +10,7 @@ end)
 QBCore.Functions.CreateCallback("qb-occasions:server:getSellerInformation", function(source, cb, citizenid)
     local src = source
 
-    exports.oxmysql:fetch('SELECT * FROM players WHERE citizenid = @citizenid', {['@citizenid'] = citizenid}, function(result)
+    exports.oxmysql:fetch('SELECT * FROM players WHERE citizenid = ?', {citizenid}, function(result)
         if result[1] ~= nil then
             cb(result[1])
         else
@@ -23,19 +23,16 @@ RegisterServerEvent('qb-occasions:server:ReturnVehicle')
 AddEventHandler('qb-occasions:server:ReturnVehicle', function(vehicleData)
     local src = source
     local Player = QBCore.Functions.GetPlayer(src)
-    local result = exports.oxmysql:fetchSync('SELECT * FROM occasion_vehicles WHERE plate=@plate AND occasionid=@occasionid', {['@plate'] = vehicleData['plate'], ['@occasionid'] = vehicleData["oid"]})
-    if result[1] ~= nil then 
+    local result = exports.oxmysql:fetchSync('SELECT * FROM occasion_vehicles WHERE plate = ? AND occasionid = ?',
+        {vehicleData['plate'], vehicleData["oid"]})
+    if result[1] ~= nil then
         if result[1].seller == Player.PlayerData.citizenid then
-            exports.oxmysql:insert('INSERT INTO player_vehicles (license, citizenid, vehicle, hash, mods, plate, state) VALUES (@license, @citizenid, @vehicle, @hash, @mods, @plate, @state)', {
-                ['@license'] = Player.PlayerData.license,
-                ['@citizenid'] = Player.PlayerData.citizenid,
-                ['@vehicle'] = vehicleData["model"],
-                ['@hash'] = GetHashKey(vehicleData["model"]),
-                ['@mods'] = vehicleData["mods"],
-                ['@plate'] = vehicleData["plate"],
-                ['@state'] = 0
-            })
-            exports.oxmysql:execute('DELETE FROM occasion_vehicles WHERE occasionid=@occasionid AND plate=@plate', {['@occasionid'] = vehicleData["oid"], ['@plate'] = vehicleData['plate']})
+            exports.oxmysql:insert(
+                'INSERT INTO player_vehicles (license, citizenid, vehicle, hash, mods, plate, state) VALUES (?)',
+                {Player.PlayerData.license, Player.PlayerData.citizenid, vehicleData["model"],
+                 GetHashKey(vehicleData["model"]), vehicleData["mods"], vehicleData["plate"], 0})
+            exports.oxmysql:execute('DELETE FROM occasion_vehicles WHERE occasionid = ? AND plate = ?',
+                {vehicleData["oid"], vehicleData['plate']})
             TriggerClientEvent("qb-occasions:client:ReturnOwnedVehicle", src, result[1])
             TriggerClientEvent('qb-occasion:client:refreshVehicles', -1)
         else
@@ -50,18 +47,18 @@ RegisterServerEvent('qb-occasions:server:sellVehicle')
 AddEventHandler('qb-occasions:server:sellVehicle', function(vehiclePrice, vehicleData)
     local src = source
     local Player = QBCore.Functions.GetPlayer(src)
-    exports.oxmysql:execute('DELETE FROM player_vehicles WHERE plate=@plate AND vehicle=@vehicle', {['@plate'] = vehicleData.plate, ['@vehicle'] = vehicleData.model})
-    exports.oxmysql:insert('INSERT INTO occasion_vehicles (seller, price, description, plate, model, mods, occasionid) VALUES (@seller, @price, @description, @plate, @model, @mods, @occasionid)', {
-        ['@seller'] = Player.PlayerData.citizenid,
-        ['@price'] = vehiclePrice,
-        ['@description'] = escapeSqli(vehicleData.desc),
-        ['@plate'] = vehicleData.plate,
-        ['@model'] = vehicleData.model,
-        ['@mods'] = json.encode(vehicleData.mods),
-        ['@occasionid'] = generateOID()
+    exports.oxmysql:execute('DELETE FROM player_vehicles WHERE plate = ? AND vehicle = ?',
+        {vehicleData.plate, vehicleData.model})
+    exports.oxmysql:insert(
+        'INSERT INTO occasion_vehicles (seller, price, description, plate, model, mods, occasionid) VALUES (?)',
+        {Player.PlayerData.citizenid, vehiclePrice, escapeSqli(vehicleData.desc), vehicleData.plate, vehicleData.model,
+         json.encode(vehicleData.mods), generateOID()})
+    TriggerEvent("qb-log:server:sendLog", Player.PlayerData.citizenid, "vehiclesold", {
+        model = vehicleData.model,
+        vehiclePrice = vehiclePrice
     })
-    TriggerEvent("qb-log:server:sendLog", Player.PlayerData.citizenid, "vehiclesold", {model=vehicleData.model, vehiclePrice=vehiclePrice})
-    TriggerEvent("qb-log:server:CreateLog", "vehicleshop", "Vehicle for Sale", "red", "**"..GetPlayerName(src) .. "** has a " .. vehicleData.model .. " priced at "..vehiclePrice)
+    TriggerEvent("qb-log:server:CreateLog", "vehicleshop", "Vehicle for Sale", "red",
+        "**" .. GetPlayerName(src) .. "** has a " .. vehicleData.model .. " priced at " .. vehiclePrice)
 
     TriggerClientEvent('qb-occasion:client:refreshVehicles', -1)
 end)
@@ -73,17 +70,18 @@ AddEventHandler('qb-occasions:server:sellVehicleBack', function(vData)
     local cid = Player.PlayerData.citizenid
     local price = math.floor(vData.price / 2)
     local plate = vData.plate
-   
+
     Player.Functions.AddMoney('bank', price)
-    TriggerClientEvent('QBCore:Notify', src, 'You have sold your car for $'..price, 'success', 5500)
-    exports.oxmysql:execute('DELETE FROM player_vehicles WHERE plate=@plate', {['@plate'] = plate, ['@citizenid'] = cid})
+    TriggerClientEvent('QBCore:Notify', src, 'You have sold your car for $' .. price, 'success', 5500)
+    exports.oxmysql:execute('DELETE FROM player_vehicles WHERE plate = ?', {plate})
 end)
 
 RegisterServerEvent('qb-occasions:server:buyVehicle')
 AddEventHandler('qb-occasions:server:buyVehicle', function(vehicleData)
     local src = source
     local Player = QBCore.Functions.GetPlayer(src)
-    local result = exports.oxmysql:fetchSync('SELECT * FROM occasion_vehicles WHERE plate=@plate AND occasionid=@occasionid', {['@plate'] = vehicleData['plate'], ['@occasionid'] = vehicleData["oid"]})
+    local result = exports.oxmysql:fetchSync('SELECT * FROM occasion_vehicles WHERE plate = ? AND occasionid = ?',
+        {vehicleData['plate'], vehicleData["oid"]})
     if result[1] ~= nil and next(result[1]) ~= nil then
         if Player.PlayerData.money.bank >= result[1].price then
             local SellerCitizenId = result[1].seller
@@ -94,41 +92,48 @@ AddEventHandler('qb-occasions:server:buyVehicle', function(vehicleData)
             Player.Functions.RemoveMoney('bank', result[1].price)
 
             -- Insert vehicle for buyer
-            exports.oxmysql:insert('INSERT INTO player_vehicles (license, citizenid, vehicle, hash, mods, plate, state) VALUES (@license, @citizenid, @vehicle, @hash, @mods, @plate, @state)', {
-                ['@license'] = Player.PlayerData.license,
-                ['@citizenid'] = Player.PlayerData.citizenid,
-                ['@vehicle'] = result[1]["model"],
-                ['@hash'] = GetHashKey(result[1]["model"]),
-                ['@mods'] = result[1]["mods"],
-                ['@plate'] = result[1]["plate"],
-                ['@state'] = 0
-            })
+            exports.oxmysql:insert(
+                'INSERT INTO player_vehicles (license, citizenid, vehicle, hash, mods, plate, state) VALUES (?)',
+                {Player.PlayerData.license, Player.PlayerData.citizenid, result[1]["model"],
+                 GetHashKey(result[1]["model"]), result[1]["mods"], result[1]["plate"], 0})
             -- Handle money transfer
             if SellerData ~= nil then
                 -- Add money for online
                 SellerData.Functions.AddMoney('bank', NewPrice)
             else
                 -- Add money for offline
-                local BuyerData = exports.oxmysql:fetchSync('SELECT * FROM players WHERE citizenid=@citizenid', {['@citizenid'] = SellerCitizenId})
+                local BuyerData = exports.oxmysql:fetchSync('SELECT * FROM players WHERE citizenid = ?',
+                    {SellerCitizenId})
                 if BuyerData[1] ~= nil then
                     local BuyerMoney = json.decode(BuyerData[1].money)
                     BuyerMoney.bank = BuyerMoney.bank + NewPrice
-                    exports.oxmysql:execute('UPDATE players SET money=@money WHERE citizenid=@citizenid', {['@money'] = json.encode(BuyerMoney), ['@citizenid'] = SellerCitizenId})
+                    exports.oxmysql:execute('UPDATE players SET money = ? WHERE citizenid = ?',
+                        {json.encode(BuyerMoney), SellerCitizenId})
                 end
             end
 
-            TriggerEvent("qb-log:server:sendLog", Player.PlayerData.citizenid, "vehiclebought", {model = result[1].model, from = SellerCitizenId, moneyType = "cash", vehiclePrice = result[1].price, plate = result[1].plate})
-            TriggerEvent("qb-log:server:CreateLog", "vehicleshop", "bought", "green", "**"..GetPlayerName(src) .. "** has bought for "..result[1].price .. " (" .. result[1].plate .. ") from **"..SellerCitizenId.."**")
+            TriggerEvent("qb-log:server:sendLog", Player.PlayerData.citizenid, "vehiclebought", {
+                model = result[1].model,
+                from = SellerCitizenId,
+                moneyType = "cash",
+                vehiclePrice = result[1].price,
+                plate = result[1].plate
+            })
+            TriggerEvent("qb-log:server:CreateLog", "vehicleshop", "bought", "green",
+                "**" .. GetPlayerName(src) .. "** has bought for " .. result[1].price .. " (" .. result[1].plate ..
+                    ") from **" .. SellerCitizenId .. "**")
             TriggerClientEvent("qb-occasions:client:BuyFinished", src, result[1])
             TriggerClientEvent('qb-occasion:client:refreshVehicles', -1)
-        
+
             -- Delete vehicle from Occasion
-            exports.oxmysql:execute('DELETE FROM occasion_vehicles WHERE plate=@plate AND occasionid=@occasionid', {['@plate'] = result[1].plate, ['@occasionid'] = result[1].occasionid})
+            exports.oxmysql:execute('DELETE FROM occasion_vehicles WHERE plate = ? AND occasionid = ?',
+                {result[1].plate, result[1].occasionid})
             -- Send selling mail to seller
             TriggerEvent('qb-phone:server:sendNewMailToOffline', SellerCitizenId, {
                 sender = "Mosleys Occasions",
                 subject = "You have sold a vehicle!",
-                message = "The "..QBCore.Shared.Vehicles[result[1].model].name.." has sold for $"..result[1].price.."!"
+                message = "The " .. QBCore.Shared.Vehicles[result[1].model].name .. " has sold for $" .. result[1].price ..
+                    "!"
             })
         else
             TriggerClientEvent('QBCore:Notify', src, 'You dont have enough money', 'error', 3500)
@@ -136,17 +141,17 @@ AddEventHandler('qb-occasions:server:buyVehicle', function(vehicleData)
     end
 end)
 
-QBCore.Functions.CreateCallback("qb-vehiclesales:server:CheckModelName",function(source,cb,plate) 
+QBCore.Functions.CreateCallback("qb-vehiclesales:server:CheckModelName", function(source, cb, plate)
     if plate then
-        local ReturnData = exports.oxmysql:scalarSync("SELECT vehicle FROM `player_vehicles` WHERE plate = @plate",{['@plate'] = plate})
+        local ReturnData = exports.oxmysql:scalarSync("SELECT vehicle FROM player_vehicles WHERE plate = ?", {plate})
         cb(ReturnData)
     end
 end)
 
 function generateOID()
-    local num = math.random(1, 10)..math.random(111, 999)
+    local num = math.random(1, 10) .. math.random(111, 999)
 
-    return "OC"..num
+    return "OC" .. num
 end
 
 function round(number)
@@ -154,6 +159,9 @@ function round(number)
 end
 
 function escapeSqli(str)
-    local replacements = { ['"'] = '\\"', ["'"] = "\\'" }
-    return str:gsub( "['\"]", replacements ) -- or string.gsub( source, "['\"]", replacements )
+    local replacements = {
+        ['"'] = '\\"',
+        ["'"] = "\\'"
+    }
+    return str:gsub("['\"]", replacements) -- or string.gsub( source, "['\"]", replacements )
 end
