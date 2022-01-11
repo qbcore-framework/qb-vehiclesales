4aab4963cac8f9a5afecaa8dfabc55d53daf3ba8
marcostom32 <62820238+marcostom32@users.noreply.github.com>
2022-01-11 01:10:42 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -9,8 +9,7 @@ ui_page 'html/ui.html'
 shared_scripts {
 	'config.lua',
 	'@qb-core/shared/locale.lua',
-	'locales/en.lua',
-	'locales/es.lua'
+	'locales/en.lua'
 }
 
 client_script 'client/main.lua'
