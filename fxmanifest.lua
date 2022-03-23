c3d27c276081639e7e96c01027f6a7ab81cfd2f2
FjamZoo <lauridspedersen83@gmail.com>
2022-03-23 23:02:21 +0100

    PolyZone and Target conversion

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -12,7 +12,16 @@ shared_scripts {
 	'locales/en.lua'
 }
 
-client_script 'client/main.lua'
+client_scripts { 
+	'@PolyZone/client.lua',
+	'@PolyZone/BoxZone.lua',
+	'@PolyZone/EntityZone.lua',
+	'@PolyZone/CircleZone.lua',
+	'@PolyZone/ComboZone.lua',
+	'client/main.lua'
+
+
+}
 server_scripts {
 	'@oxmysql/lib/MySQL.lua',
 	'server/main.lua'
