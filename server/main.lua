1fd7e4504078465855ce3dac6900997f167a5ccf
Kakarot <57848836+GhzGarage@users.noreply.github.com>
2022-05-19 01:01:29 -0500

    MySQL Updates

--- server/main.lua
+++ server/main.lua
@@ -19,7 +19,7 @@ end
 -- Callbacks
 
 QBCore.Functions.CreateCallback('qb-occasions:server:getVehicles', function(_, cb)
-    local result = MySQL.Sync.fetchAll('SELECT * FROM occasion_vehicles', {})
+    local result = MySQL.query.await('SELECT * FROM occasion_vehicles', {})
     if result[1] then
         cb(result)
     else
@@ -28,7 +28,7 @@ QBCore.Functions.CreateCallback('qb-occasions:server:getVehicles', function(_, c
 end)
 
 QBCore.Functions.CreateCallback("qb-occasions:server:getSellerInformation", function(_, cb, citizenid)
-    MySQL.Async.fetchAll('SELECT * FROM players WHERE citizenid = ?', {citizenid}, function(result)
+    MySQL.query('SELECT * FROM players WHERE citizenid = ?', {citizenid}, function(result)
         if result[1] then
             cb(result[1])
         else
@@ -39,7 +39,7 @@ end)
 
 QBCore.Functions.CreateCallback("qb-vehiclesales:server:CheckModelName", function(_, cb, plate)
     if plate then
-        local ReturnData = MySQL.Sync.fetchScalar("SELECT vehicle FROM player_vehicles WHERE plate = ?", {plate})
+        local ReturnData = MySQL.scalar.await("SELECT vehicle FROM player_vehicles WHERE plate = ?", {plate})
         cb(ReturnData)
     end
 end)
@@ -49,14 +49,14 @@ end)
 RegisterNetEvent('qb-occasions:server:ReturnVehicle', function(vehicleData)
     local src = source
     local Player = QBCore.Functions.GetPlayer(src)
-    local result = MySQL.Sync.fetchAll('SELECT * FROM occasion_vehicles WHERE plate = ? AND occasionid = ?', {vehicleData['plate'], vehicleData["oid"]})
+    local result = MySQL.query.await('SELECT * FROM occasion_vehicles WHERE plate = ? AND occasionid = ?', {vehicleData['plate'], vehicleData["oid"]})
     if result[1] then
         if result[1].seller == Player.PlayerData.citizenid then
-            MySQL.Async.insert(
+            MySQL.insert(
                 'INSERT INTO player_vehicles (license, citizenid, vehicle, hash, mods, plate, state) VALUES (?, ?, ?, ?, ?, ?, ?)',
                 {Player.PlayerData.license, Player.PlayerData.citizenid, vehicleData["model"],
                  GetHashKey(vehicleData["model"]), vehicleData["mods"], vehicleData["plate"], 0})
-            MySQL.Async.execute('DELETE FROM occasion_vehicles WHERE occasionid = ? AND plate = ?',
+            MySQL.query('DELETE FROM occasion_vehicles WHERE occasionid = ? AND plate = ?',
                 {vehicleData["oid"], vehicleData['plate']})
             TriggerClientEvent("qb-occasions:client:ReturnOwnedVehicle", src, result[1])
             TriggerClientEvent('qb-occasion:client:refreshVehicles', -1)
@@ -71,8 +71,8 @@ end)
 RegisterNetEvent('qb-occasions:server:sellVehicle', function(vehiclePrice, vehicleData)
     local src = source
     local Player = QBCore.Functions.GetPlayer(src)
-    MySQL.Async.execute('DELETE FROM player_vehicles WHERE plate = ? AND vehicle = ?',{vehicleData.plate, vehicleData.model})
-    MySQL.Async.insert('INSERT INTO occasion_vehicles (seller, price, description, plate, model, mods, occasionid) VALUES (?, ?, ?, ?, ?, ?, ?)',{Player.PlayerData.citizenid, vehiclePrice, escapeSqli(vehicleData.desc), vehicleData.plate, vehicleData.model,json.encode(vehicleData.mods), generateOID()})
+    MySQL.query('DELETE FROM player_vehicles WHERE plate = ? AND vehicle = ?',{vehicleData.plate, vehicleData.model})
+    MySQL.insert('INSERT INTO occasion_vehicles (seller, price, description, plate, model, mods, occasionid) VALUES (?, ?, ?, ?, ?, ?, ?)',{Player.PlayerData.citizenid, vehiclePrice, escapeSqli(vehicleData.desc), vehicleData.plate, vehicleData.model,json.encode(vehicleData.mods), generateOID()})
     TriggerEvent("qb-log:server:CreateLog", "vehicleshop", "Vehicle for Sale", "red","**" .. GetPlayerName(src) .. "** has a " .. vehicleData.model .. " priced at " .. vehiclePrice)
     TriggerClientEvent('qb-occasion:client:refreshVehicles', -1)
 end)
@@ -90,20 +90,20 @@ RegisterNetEvent('qb-occasions:server:sellVehicleBack', function(vehData)
     local payout = price / 2
     Player.Functions.AddMoney('bank', payout)
     TriggerClientEvent('QBCore:Notify', src, Lang:t('success.sold_car_for_price', { value = payout }), 'success', 5500)
-    MySQL.Async.execute('DELETE FROM player_vehicles WHERE plate = ?', {plate})
+    MySQL.query('DELETE FROM player_vehicles WHERE plate = ?', {plate})
 end)
 
 RegisterNetEvent('qb-occasions:server:buyVehicle', function(vehicleData)
     local src = source
     local Player = QBCore.Functions.GetPlayer(src)
-    local result = MySQL.Sync.fetchAll('SELECT * FROM occasion_vehicles WHERE plate = ? AND occasionid = ?',{vehicleData['plate'], vehicleData["oid"]})
+    local result = MySQL.query.await('SELECT * FROM occasion_vehicles WHERE plate = ? AND occasionid = ?',{vehicleData['plate'], vehicleData["oid"]})
     if result[1] and next(result[1]) then
         if Player.PlayerData.money.bank >= result[1].price then
             local SellerCitizenId = result[1].seller
             local SellerData = QBCore.Functions.GetPlayerByCitizenId(SellerCitizenId)
             local NewPrice = math.ceil((result[1].price / 100) * 77)
             Player.Functions.RemoveMoney('bank', result[1].price)
-            MySQL.Async.insert(
+            MySQL.insert(
                 'INSERT INTO player_vehicles (license, citizenid, vehicle, hash, mods, plate, state) VALUES (?, ?, ?, ?, ?, ?, ?)', {
                     Player.PlayerData.license,
                     Player.PlayerData.citizenid, result[1]["model"],
@@ -115,17 +115,17 @@ RegisterNetEvent('qb-occasions:server:buyVehicle', function(vehicleData)
             if SellerData then
                 SellerData.Functions.AddMoney('bank', NewPrice)
             else
-                local BuyerData = MySQL.Sync.fetchAll('SELECT * FROM players WHERE citizenid = ?',{SellerCitizenId})
+                local BuyerData = MySQL.query.await('SELECT * FROM players WHERE citizenid = ?',{SellerCitizenId})
                 if BuyerData[1] then
                     local BuyerMoney = json.decode(BuyerData[1].money)
                     BuyerMoney.bank = BuyerMoney.bank + NewPrice
-                    MySQL.Async.execute('UPDATE players SET money = ? WHERE citizenid = ?', {json.encode(BuyerMoney), SellerCitizenId})
+                    MySQL.update('UPDATE players SET money = ? WHERE citizenid = ?', {json.encode(BuyerMoney), SellerCitizenId})
                 end
             end
             TriggerEvent("qb-log:server:CreateLog", "vehicleshop", "bought", "green", "**" .. GetPlayerName(src) .. "** has bought for " .. result[1].price .. " (" .. result[1].plate ..") from **" .. SellerCitizenId .. "**")
             TriggerClientEvent("qb-occasions:client:BuyFinished", src, result[1])
             TriggerClientEvent('qb-occasion:client:refreshVehicles', -1)
-            MySQL.Async.execute('DELETE FROM occasion_vehicles WHERE plate = ? AND occasionid = ?',{result[1].plate, result[1].occasionid})
+            MySQL.query('DELETE FROM occasion_vehicles WHERE plate = ? AND occasionid = ?',{result[1].plate, result[1].occasionid})
             TriggerEvent('qb-phone:server:sendNewMailToOffline', SellerCitizenId, {
                 sender = Lang:t('mail.sender'),
                 subject = Lang:t('mail.subject'),
