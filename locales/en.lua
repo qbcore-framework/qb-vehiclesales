2332d07d05c88458ea9b2dd8214b469e19204fe7
Martin Riggs <63644849+wanderrer@users.noreply.github.com>
2022-08-26 10:38:16 +0300

    Update en.lua

--- locales/en.lua
+++ locales/en.lua
@@ -4,7 +4,7 @@ local Translations = {
         vehicle_does_not_exist = 'Vehicle does not exist',
         not_enough_money = 'You dont have enough money',
         finish_payments = 'You must finish paying off this vehicle, Before you can sell it..',
-        no_space_on_lot = 'There is not space for your car on the lot!'
+        no_space_on_lot = 'There is not space for your car on the lot!',
         not_in_veh = 'You are not in a vehicle!',
         not_for_sale = 'This vehicle is NOT for sale!',
     },
@@ -22,7 +22,7 @@ local Translations = {
     success = {
         sold_car_for_price = 'You have sold your car for $%{value}',
         car_up_for_sale = 'Your car has been put up for sale! Price - $%{value}',
-        vehicle_bought = 'Vehicle Bought'
+        vehicle_bought = 'Vehicle Bought',
     },
     info = {
         confirm_cancel = '~g~Y~w~ - Confirm / ~r~N~w~ - Cancel ~g~',
@@ -34,18 +34,18 @@ local Translations = {
         model_price = '%{value}, Price: ~g~$%{value2}',
         are_you_sure = 'Are you sure you no longer want to sell your vehicle?',
         yes_no = '[~g~7~w~] - Yes | [~r~8~w~] - No',
-        place_vehicle_for_sale = '[~g~E~w~] - Place Vehicle For Sale By Owner'
+        place_vehicle_for_sale = '[~g~E~w~] - Place Vehicle For Sale By Owner',
     },
     charinfo = {
         firstname = 'not',
         lastname = 'known',
         account = 'Account not known..',
-        phone = 'telephone number not known..'
+        phone = 'telephone number not known..',
     },
     mail = {
         sender = 'Larrys RV Sales',
         subject = 'You have sold a vehicle!',
-        message = 'You made $%{value} from the sale of your %{value2}.'
+        message = 'You made $%{value} from the sale of your %{value2}.',
     }
 }
 
